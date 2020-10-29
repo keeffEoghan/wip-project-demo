51d5b2b02cabd0735ba4f2832ed1335c044e5563
Eoghan O'Keeffe <keeffeoghan@gmail.com>
2020-10-29 12:17:56 +0200

    Add files via upload

---
+++ assets/holds/meta.js
@@ -0,0 +1,8 @@
+export const dataPath = './src/assets/holds/paths.json';
+export const svgPath = './src/assets/holds/svg/';
+export const sdfPath = './src/assets/holds/sdf/';
+export const size = [2172, 2663];
+export const scale = 0.8;
+export const radius = 1;
+export const cutoff = 0.1;
+export const channel = 3;
